5d461126e709059db2dbf8995f9b2eaaf9d1ce08
slvic <vkenrik@gmail.com>
2022-02-01 14:49:34 +0300

    Add delivery service

--- internal/service/deliveries/deliveries.go
+++ internal/service/deliveries/deliveries.go
@@ -8,23 +8,61 @@ import (
 	"time"
 )
 
-func run(ctx context.Context) error {
-	connect, err := nats.Connect(nats.DefaultURL)
+type Stream struct {
+	subscriptions map[string]*nats.Subscription
+	jetstream     nats.JetStream
+}
+
+func NewStream(url string) (*Stream, error) {
+	connect, err := nats.Connect(url)
 	if err != nil {
-		return fmt.Errorf("connect: %v", err.Error())
+		return &Stream{}, fmt.Errorf("connect: %v", err.Error())
 	}
 
 	stream, err := connect.JetStream(nats.PublishAsyncMaxPending(256))
 	if err != nil {
-		return fmt.Errorf("get JetStream: %v", err.Error())
+		return &Stream{}, fmt.Errorf("get JetStream: %v", err.Error())
+	}
+	return &Stream{jetstream: stream}, nil
+}
+
+func (s *Stream) Subscribe(subject string) error {
+	if _, ok := s.subscriptions[subject]; ok {
+		return fmt.Errorf("subscription already exists")
 	}
 
-	sub, err := stream.SubscribeSync("ORDERS.*")
+	sub, err := s.jetstream.SubscribeSync(subject)
 	if err != nil {
-		return fmt.Errorf("subscribe: %v", err.Error())
+		return fmt.Errorf("cound not subscribe to a subject: %v", err)
 	}
-	defer sub.Unsubscribe()
-	defer sub.Drain()
+	s.subscriptions[subject] = sub
+	return nil
+}
+
+func (s *Stream) Publish(subject string, message string) error {
+	_, err := s.jetstream.PublishAsync(subject, []byte(message))
+	if err != nil {
+		return fmt.Errorf("could not publish a message: %v", err)
+	}
+	return nil
+}
+
+func (s *Stream) Run(ctx context.Context) error {
+	var err error
+	defer func(subscriptions map[string]*nats.Subscription) {
+		for _, sub := range subscriptions {
+			if unSubErr := sub.Unsubscribe(); unSubErr != nil {
+				err = fmt.Errorf("could not unsubscribe properly: %v", unSubErr)
+			}
+		}
+	}(s.subscriptions)
+	defer func(subscriptions map[string]*nats.Subscription) {
+		for _, sub := range subscriptions {
+			if drainErr := sub.Drain(); drainErr != nil {
+				err = fmt.Errorf("could not unsubscribe properly: %v", drainErr)
+			}
+		}
+	}(s.subscriptions)
 
 streamLoop:
 	for {
@@ -33,18 +71,22 @@ streamLoop:
 			fmt.Println("done")
 			break streamLoop
 		default:
-			msg, err := sub.NextMsg(time.Second)
-			if err == nats.ErrTimeout {
-				continue
+			for _, sub := range s.subscriptions {
+				msg, err := sub.NextMsg(time.Second)
+				if err == nats.ErrTimeout {
+					continue
+				}
+				if err != nil {
+					// migrate to zap logger
+					_, _ = fmt.Fprintf(os.Stderr, "next msg: %s", err.Error())
+				}
+				_ = msg.Ack()
+				fmt.Println("new message:", string(msg.Data))
 			}
-			if err != nil {
-				// migrate to zap logger
-				_, _ = fmt.Fprintf(os.Stderr, "next msg: %s", err.Error())
-			}
-			_ = msg.Ack()
-			fmt.Println("new message:", string(msg.Data))
 		}
 	}
-
+	if err != nil {
+		return err
+	}
 	return nil
 }
