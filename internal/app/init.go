2328f515f8b3eec3eedc6038a40a7687690ab4c8
slvic <vkenrik@gmail.com>
2022-02-02 19:52:47 +0300

    Add load stream messages from db

--- internal/app/init.go
+++ internal/app/init.go
@@ -9,11 +9,23 @@ import (
 	worker "github.com/slvic/nats-service/internal/nats"
 	"github.com/slvic/nats-service/internal/service/deliveries"
 	"github.com/slvic/nats-service/internal/store/memory"
+	"github.com/slvic/nats-service/internal/store/persistent"
 	"go.uber.org/zap"
 )
 
 func Initialize(ctx context.Context, logger *zap.Logger, store *memory.Store) error {
-	storeService := deliveries.New(store)
+	// add config
+	database, err := persistent.New("postgres", "user=postgres dbname=nats-service password=postgres port=5433 sslmode=disable")
+	if err != nil {
+		return fmt.Errorf("could not create new database: %s", err.Error())
+	}
+
+	err = LoadOrdersFromDB(database, store)
+	if err != nil {
+		return fmt.Errorf("could not load orders from db: %s", err.Error())
+	}
+
+	storeService := deliveries.New(store, database)
 
 	newWorker, err := worker.New(nats.DefaultURL, logger)
 	if err != nil {
@@ -38,6 +50,18 @@ func Initialize(ctx context.Context, logger *zap.Logger, store *memory.Store) er
 	err = server.Start()
 	if err != nil {
 		return err
+	}
+	return nil
+}
+
+func LoadOrdersFromDB(db *persistent.Database, store *memory.Store) error {
+	allMessages, err := db.GetAll()
+	if err != nil {
+		return fmt.Errorf("could not get all stream messages: %s", err.Error())
+	}
+
+	for _, message := range allMessages {
+		store.Set(message.Id, message.Data)
 	}
 	return nil
 }
