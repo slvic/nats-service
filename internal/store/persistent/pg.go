2b664e9e1c0afa70764ab8c312eef7f422d29948
slvic <vkenrik@gmail.com>
2022-02-02 19:49:03 +0300

    Update persistent

--- internal/store/persistent/pg.go
+++ internal/store/persistent/pg.go
@@ -3,8 +3,8 @@ package persistent
 import (
 	"database/sql"
 	"fmt"
+
 	_ "github.com/lib/pq"
-	"github.com/slvic/nats-service/internal/service"
 	"github.com/slvic/nats-service/internal/types"
 )
 
@@ -12,7 +12,7 @@ type Database struct {
 	pg *sql.DB
 }
 
-func NewDb(driver string, connection string) (*Database, error) {
+func New(driver string, connection string) (*Database, error) {
 	db, err := sql.Open(driver, connection)
 	if err != nil {
 		return nil, fmt.Errorf("could not open db: %v", err)
@@ -22,60 +22,37 @@ func NewDb(driver string, connection string) (*Database, error) {
 	return database, nil
 }
 
-func (d *Database) SaveOrUpdateMany(data []string) error {
-	orders, err := service.UnmarshalAndValidate(data)
-	if err != nil {
-		return fmt.Errorf("could not unmarshal or validate while save or update many: %v", err)
-	}
+func (d *Database) SaveOrUpdate(order types.Order, rawOrder []byte) error {
+	queryString := `INSERT INTO orders (id, data) VALUES ($1, $2) ON CONFLICT (id) DO UPDATE SET data = EXCLUDED.data`
+	values := []interface{}{order.Uid, rawOrder}
 
-	queryString := `INSERT INTO order(id, data) VALUES `
-	values := []interface{}{}
-	for _, row := range orders {
-		queryString += `(?, ?),`
-		values = append(values, row.Uid, row)
-	}
-	queryString = queryString[:len(queryString)-1]
-	queryString += `ON CONFLICT (id) DO UPDATE SET data = EXCLUDED.data`
-
-	_, err = d.pg.Query(queryString, values)
-	if err != nil {
-		return fmt.Errorf("could not execute save or update many query: %v", err)
-	}
-	return nil
-}
-
-func (d *Database) SaveOrUpdate(data string) error {
-	order, err := service.UnmarshalAndValidate([]string{data})
-	if err != nil {
-		return fmt.Errorf("could not unmarshal or validate while save or update: %v", err)
-	}
-
-	queryString := `INSERT INTO order(id, data) VALUES (?, ?) ON CONFLICT (id) DO UPDATE SET data = EXCLUDED.data`
-	values := []interface{}{order[0].Uid, order[0]}
-
-	_, err = d.pg.Query(queryString, values)
+	_, err := d.pg.Query(queryString, values...)
 	if err != nil {
 		return fmt.Errorf("could not execute save or update query: %v", err)
 	}
 	return nil
 }
 
-func (d *Database) GetAll() ([]types.Order, []error) {
-	var errors []error
-	var orders []types.Order
-	var dbOrder *types.Order
-	queryString := `SELECT data FROM orders`
+func (d *Database) GetAll() ([]types.Message, error) {
+	var orders []types.Message
+	dbOrder := new(types.Message)
+	queryString := `SELECT * FROM orders`
 
 	rows, err := d.pg.Query(queryString)
 	if err != nil {
-		return nil, []error{fmt.Errorf("could not execute get all query: %v", err)}
+		return nil, fmt.Errorf("could not execute get all query: %v", err)
 	}
+	defer func(rows *sql.Rows) {
+		rowsCloseErr := rows.Close()
+		if rowsCloseErr != nil {
+			err = fmt.Errorf("could not close rows: %s", rowsCloseErr.Error())
+		}
+	}(rows)
 
 	for rows.Next() {
-		err := rows.Scan(dbOrder)
+		err := rows.Scan(&dbOrder.Id, &dbOrder.Data)
 		if err != nil {
-			errors = append(errors, err)
-			continue
+			return nil, err
 		}
 		orders = append(orders, *dbOrder)
 	}
